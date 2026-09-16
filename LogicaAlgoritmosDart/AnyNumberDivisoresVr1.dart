49568cdda38449c2abc8d0253d33c8d67d734d36
Edward Everest <castroeduardoavelino@gmail.com>
2026-09-16 18:29:23 +0100

    Done the error Correction about twince Startup question

--- LogicaAlgoritmosDart/AnyNumberDivisoresVr1.dart
+++ LogicaAlgoritmosDart/AnyNumberDivisoresVr1.dart
@@ -1,62 +1,51 @@
 /*This code calculates the divisors of any positive integer number greater than zero entered by the user.
 Version 1.0: Initial idea */
 
-import 'dart:io'; 
-void main(){
+import 'dart:io';
 
+void main() {
   //Program startup
   print("Welcome to the Divisor Calculator!\n");
-  print("If you want start, press Enter:");
+  print("If you want start, press Enter twince:");
   stdin.readLineSync()!;
 
-  if(stdin.readLineSync() != null){
-    int anynumber=inicializing(); 
-    while (anynumber > 0){
-    int anynumber =inicializing(); 
-    calculateDivisores(anynumber);  
-
-    if (anynumber>0){
-    print("Do you want to continue? (y/n)");
-    String continuePlay= stdin.readLineSync()!.toLowerCase(); 
-    if (continuePlay == 'y' && anynumber > 0){
-        int anynumber = inicializing(); 
-        calculateDivisores(anynumber);
-        
+  if (stdin.readLineSync() != null) {
+    while (true) {
+      int anynumber = inicializing();
+      calculateDivisores(anynumber);
+      if (anynumber > 0) {
+        print("Do you want to continue? (y/n)");
+        String continuePlay = stdin.readLineSync()!.toLowerCase();
+        if (continuePlay == 'y' && anynumber > 0) {
+          int anynumber = inicializing();
+          calculateDivisores(anynumber);
+        } else
+          print("Thank you for using the program!");
+      }
     }
-    else
-      print("Thank you for using the program!");
-  }
-  }
-  //Loop to check for the error
-  while (anynumber <= 0){
-    anynumber = inicializing(); 
-  
-  }
-
   }
 }
-int calculateDivisores(int numberTyped){
-  
-  int divisors=0; 
-  if (numberTyped > 0){
-    for (int i=1; i<=numberTyped; i++){
-      if (numberTyped % i== 0 ){
+
+int calculateDivisores(int numberTyped) {
+  int divisors = 0;
+  if (numberTyped > 0) {
+    for (int i = 1; i <= numberTyped; i++) {
+      if (numberTyped % i == 0) {
         print(i);
         divisors++;
       }
     }
-  
+
     print("The divisors of $numberTyped are: $divisors \n");
-    
   }
- return divisors; 
+  return divisors;
 }
 
-int inicializing(){
+int inicializing() {
   print("Enter a positive integer greater than zero:\n");
-  int usernamber = int.parse(stdin.readLineSync()!); 
-  if (usernamber <=0){
+  int usernamber = int.parse(stdin.readLineSync()!);
+  if (usernamber <= 0) {
     print("Please enter a positive integer greater than zero!\n ");
   }
-return usernamber;
+  return usernamber;
 }
